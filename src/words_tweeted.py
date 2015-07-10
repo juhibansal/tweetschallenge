808f0b8e4703339febe76f57a4aa36c7cd4ba332
juhibansal <juhi.bansal@gmail.com>
2015-07-10 15:18:21 -0700

    Update words_tweeted.py

--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,10 +1,9 @@
 # $Id: words_tweeted.py,v 1.3 2015/07/08 jbansal $
 
 """
-Creates a list of words tweeted and count the occurence
-
 Script Usage: 
-  words_tweeted.py tweet_input tweet_output/ft1.txt
+  words_tweeted.py tweet_input tweet_output
+######Missing some inputs##########
 """
 
 # import python packages
@@ -21,15 +20,19 @@ debug_print = 0
 # Function
 ##############################
 # --------------------------------------------------
-def words_tweeted(dir_input,file_output):
+def words_tweeted(dir_input,dir_output):
 
+	# get all txt files in input directory
 	list_inputs=glob('%s/*.txt'%dir_input)
+	# If no txt files in input directory, exit
 	if not list_inputs: 
 		print 'Error: Directory %s has no text files. '%dir_input
 		exit (1)
-	ft1out = open('%s'%file_output,'w')
+	# Open output file to write 
+	ft1out = open('%s/ft1.txt'%dir_output,'w')
 	dict_tweets  = {}
 	k = 0
+	# Read each input file line by line 
 	for ifile in list_inputs:
 		for line in file(ifile):
 			line = line.strip()
@@ -44,6 +47,7 @@ def words_tweeted(dir_input,file_output):
 				continue
 	
 	
+	# Sort and print the list 
 	for x in sorted(dict_tweets):
 		print >> ft1out, x.ljust(k+2, ) + '%5g'%dict_tweets[x] 
 	if debug_print: print pformat(dict_tweets)
@@ -56,18 +60,19 @@ def words_tweeted(dir_input,file_output):
 if __name__ == "__main__":
 
 	# read in configure info
-	if len(argv) < 2:
+	if len(argv) < 3:
 		print __doc__
 		exit (1)
 
 	dir_input = argv[1]
-	file_output = argv[2]
+	dir_output = argv[2]
 
+	# check if input directory exists, if not exit 
 	if not os.path.isdir(dir_input): 
 		print 'Error: Directory %s is not found. '%dir_input
 		exit (1)
-	loc = file_output.split('/')
-	dir_output = '/'.join(loc[:len(loc)-1])
+	# check if onput directory exists, if not create one 
 	if not os.path.isdir(dir_output): os.makedirs(dir_output)
 
-	words_tweeted(dir_input,file_output)
+	# call funtion to get frequency of words being tweeted 
+	words_tweeted(dir_input,dir_output)
