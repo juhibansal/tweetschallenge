e5b8cb5e2de8a225933acc34a6d1e192ab1253b9
juhibansal <juhi.bansal@gmail.com>
2015-07-10 15:32:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/words_tweeted.py ./tweet_input ./tweet_output/ft1.txt
-python ./src/median_unique.py ./tweet_input ./tweet_output/ft2.txt
+python ./src/words_tweeted.py ./tweet_input ./tweet_output
+python ./src/median_unique.py ./tweet_input ./tweet_output
