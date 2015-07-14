0d0defe6cbd00616e36ba85396bd89fe7a12fb2b
juhibansal <juhi.bansal@gmail.com>
2015-07-14 09:35:02 -0700

    Update median_unique.py

--- src/median_unique.py
+++ src/median_unique.py
@@ -3,7 +3,6 @@
 """
 Script Usage: 
   median_unique.py tweet_input tweet_output
-
 """
 
 # import python packages
@@ -13,7 +12,6 @@ from os.path import isfile,isdir
 from pprint import pformat
 from sys import argv, exit
 from glob import glob
-debug_print = 0
 
 ##############################
 # Function
@@ -29,77 +27,64 @@ def median_unique(dir_input,dir_output):
 		exit (1)
 	# Open output file to write 
 	ft2out = open('%s/ft2.txt'%dir_output,'w')
-	#create a dictionary to store number of unique words, number of times the same count of words has appeared
-	dict_tweets  = {}
-	dict_tweets_copy  = {}
+	#create a list to store number of unique words, number of times the same count of words has appeared
+	#index of the list will be count of words and frequency of occurence pf the same count will be value of list at that count index
+	#Assumption is tweet can not have more than 140 character, so list will not have length more than 140. Value can be any huge number 
+	list_tweets  = [0]*140
 	# Read each input file line by line 
 	for ifile in list_inputs:
 		for line in file(ifile):
 			line = line.strip()
 			if not line: continue
-
 			fields = line.split()
 			#Count the number of unique words from a line
 			fields = list(set(fields))
-			#Store the word count as key and number of times the same key appear as value
-			#Keep incrementing the value of the key in the dictionary if the same count appears
-			#Storing frequency based dictionay is usedful to support of large amount of data, 
-			#since twitter can have maximum 40 words, size of directory will never be more than 40.
-			#With every additional tweets only frequency will need to be incremented. Size of dictionary wont increase
-			#Had to keep a copy of parent directory, since parent directory is getting overwritten in recursive function
-			if not len(fields) in dict_tweets_copy: dict_tweets_copy[len(fields)] = 1
-			else: dict_tweets_copy[len(fields)] = dict_tweets_copy[len(fields)] + 1
-			dict_tweets = dict_tweets_copy.copy()
+			count = len(fields)
+			#Increment the value in list if the same count occurs again
+			list_tweets[count] = list_tweets[count] + 1
 			# Call median function to calculate median and print with maximum 2 significant digits
-			print >> ft2out, ('%.2f'%(median(dict_tweets))).rstrip('0').rstrip('.')
+			print >> ft2out, ('%.2f'%(median(list_tweets))).rstrip('0').rstrip('.')
 			continue
 	
 	return
 
 # function to calculate median
-def median(dict_tweets):
-	flag = 0
-	#recursive function to elimitate first or end key of dictionary based on the frequency of the key
-	#Perform recursion only of dictionary has more than 1 key or more than 2 keys with different frequencies
-	while (len(dict_tweets) > 1):
-		if (len(dict_tweets) ==2 and dict_tweets.values()[0]==dict_tweets.values()[1]):
-			flag = 1
-			break
-		dict_tweets = eliminate(dict_tweets)
-	
-	#calculate median, if two keys left then take median as addition of 2
-	if flag == 1: median = (dict_tweets.keys()[0] + dict_tweets.keys()[1])/2.0
-	#else take the only key left
-	else: median = dict_tweets.keys()[0]
+def median(list_tweets):
+	#Total count of tweets is given by addition of all values in array.
+	#Median will be 
+		#list index at count/2 if count is odd and 
+		#average of list indexs at int(count/2) and int(count/2)+1 if count it even
+    
+	total = sum(list_tweets)
+	total_tillnow = 0
+	median=0
+	medianx=0
+	mediany=0
+	#Get list index at count/2 for odd number of tweets
+	if (total%2!=0):
+		for idx, val in enumerate(list_tweets):
+			if val != 0:
+				total_tillnow = total_tillnow + val
+				if(total_tillnow >= int(total/2)+1):
+					median = idx
+					break
+	#for Even count 2 possibilities
+	    #list index at count/2 and count/2+1 is same then median will be index
+	    #list index different at count/2 and count/2+1 then median will averages of 2 indexes
+	if (total%2==0):
+		for idx, val in enumerate(list_tweets):
+			if val != 0:
+				total_tillnow = total_tillnow + val
+				if(total_tillnow >= int(total/2)):
+					if(total_tillnow >= int(total/2)+1):
+						median = idx
+						break
+					else:
+						medianx = idx
+						mediany = next((x for (x, y) in list(enumerate(list_tweets))[idx+1:] if y),idx)
+						median = float(medianx+mediany)/2.0
+						break
 	return median
-
-#elimiate the first or last end key of dictionary, if the first key value is larger then remove last key and vice versa and subtract the key value
-# if first and last key value are same then remove both from dictionary
-def eliminate(dict_tweets):
-	#sorting dictionary by keys
-	sorted_tweets = sorted(dict_tweets)
-	dict_len = len(dict_tweets)
-	#get first key and last key
-	firstkey = sorted_tweets[0]
-	lastkey = sorted_tweets[dict_len-1]
-	#get first key value and last key value
-	firstvalue = dict_tweets[firstkey]
-	lastvalue = dict_tweets[lastkey]
-	#if first key value is more remove last key and subtract last key value from first one, i.e. change the frequency of first key occurence
-	if firstvalue > lastvalue:
-		firstvalue = firstvalue - lastvalue
-		dict_tweets[firstkey]=firstvalue
-		if lastkey in dict_tweets: del dict_tweets[lastkey]
-	#if first key value is less remove first key and subtract first key value from last key value
-	elif firstvalue < lastvalue:
-		lastvalue = lastvalue - firstvalue
-		dict_tweets[lastkey]=lastvalue
-		if firstkey in dict_tweets: del dict_tweets[firstkey]
-	#if first key value = last key value remove both
-	elif firstvalue == lastvalue:
-		if firstkey in dict_tweets: del dict_tweets[firstkey]
-		if lastkey in dict_tweets: del dict_tweets[lastkey]
-	return(dict_tweets)
 	
 ##############################
 # Main
